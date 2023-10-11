048d37a4549bf5504b7e7a9b8547eb18010baf0a
aecong <dkdus78655@gmail.com>
2023-10-11 21:07:07 +0900

    Drill09 제출 (2022182015 박아연)

--- boy.py
+++ boy.py
@@ -1,6 +1,6 @@
 # 이것은 각 상태들을 객체로 구현한 것임.
 
-from pico2d import load_image, SDL_KEYDOWN, SDLK_SPACE, get_time, SDLK_RIGHT, SDLK_LEFT, SDL_KEYUP
+from pico2d import load_image, SDL_KEYDOWN, SDLK_SPACE, get_time, SDLK_RIGHT, SDLK_LEFT, SDL_KEYUP, SDLK_a
 import math
 
 
@@ -28,6 +28,38 @@ def left_up(e):
     return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_LEFT
 
 
+def auto(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_a
+
+
+class AutoRun:
+    @staticmethod
+    def enter(boy, e):
+        if auto(e):
+            boy.action = 1
+            boy.dir = 1
+            boy.auto_start_time = get_time()
+    @staticmethod
+    def exit(boy, e):
+        pass
+
+    @staticmethod
+    def do(boy):
+        boy.frame = (boy.frame + 1) % 8
+
+        if boy.x > 750:
+            boy.dir, boy.action = -1, 0
+        elif boy.x < 50:
+            boy.dir, boy.action = 1, 1
+
+        boy.x += boy.dir * 25
+
+        if get_time() - boy.auto_start_time > 5:
+            boy.state_machine.handle_event(('TIME_OUT', 0))
+
+    @staticmethod
+    def draw(boy):
+        boy.image.clip_draw(boy.frame * 100, boy.action * 100, 100, 100, boy.x, boy.y + 25, 200, 200)
 class Idle:
     @staticmethod
     def enter(boy, e):
@@ -106,8 +138,9 @@ class StateMachine:
         self.cur_state = Idle
         self.transitions = {
             Sleep: {right_down: Run, left_down: Run, right_up: Run, left_up: Run, space_down: Idle},  # dictionary : key 로 부터 value 를 찾아냄
-            Idle: {right_down: Run, left_down: Run, left_up: Run, right_up: Run, time_out: Sleep},
-            Run: {right_down: Idle, left_down: Idle, right_up: Idle, left_up: Idle}
+            Idle: {right_down: Run, left_down: Run, left_up: Run, right_up: Run, time_out: Sleep, auto: AutoRun},
+            Run: {right_down: Idle, left_down: Idle, right_up: Idle, left_up: Idle},
+            AutoRun: {time_out: Idle}
         }
 
     def start(self):
